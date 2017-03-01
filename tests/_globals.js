8185c671ccf8bf3c195ceb0ba822d1284d7baf65
Gustavo Felizola <gfelizola@gmail.com>
2017-03-01 19:23:09 -0300

    initial commit with functional structure

---
+++ tests/_globals.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // before: function(browser) {
+    //     console.log('Iniciandos testes...');
+    // },
+
+    // after: function(browser) {
+    //     console.log('Testes finalizados...');
+    // }
+};
---
+++ tests/testuser-login.js
@@ -0,0 +1,18 @@
+module.exports = {
+    '@disable': false,
+    '@tags': ['login'],
+    'Login to testuser.ml.com' : function (browser) {
+        browser
+            .url('http://testuser.ml.com')
+            .waitForElementVisible('body', 5000)
+            .assert.title('Login')
+            .assert.visible('input#session_username')
+            .setValue('input#session_username', process.env.MELI_USER)
+            .setValue('input#session_password', process.env.MELI_PASS)
+            .waitForElementVisible('input[type=submit]', 1000)
+            .click('input[type=submit]')
+            .pause(2000)
+            .assert.title('Test User')
+            .end();
+    }
+};
